101a8c5d83ec8b8f8044e6e07f7510230dd32876
Jake Howard <git@theorangeone.net>
2016-10-12 17:46:15 +0100

    create basic api view

--- project/images/views.py
+++ project/images/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ImageAPIView(GenericAPIView):
+    def get(self, request, width, height):
+        return Response({
+            'width': width,
+            'height': height
+        })
